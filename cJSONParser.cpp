0b55d7515fa74e8d28f41618e5d0a72c679bc0f9
thinhnguyen <nguyentrongthinhit@gmail.com>
2019-12-25 10:08:47 +0700

    Enable gwuid when receive command from server
    Signed-off-by: thinhnguyen <nguyentrongthinhit@gmail.com>

--- cJSONParser.cpp
+++ cJSONParser.cpp
@@ -23,28 +23,28 @@ QByteArray cJSONParser::rawCommandFromServer(QByteArray jsonFromServer)
          qDebug() << "Null JsonDocument";
     if (jsonDoc.isObject()) {
         QJsonObject jsonObj = jsonDoc.object();
-//        quint32 gwuid = static_cast<quint32>(jsonObj.take("gwuid").toInt(0));
-//        qDebug() << "Gateway UID From Server: " << gwuid;
-//        if (cGatewayUID::getGateWayUID() == gwuid) {
-        QJsonArray jsonArray = jsonObj.take("command").toArray();
-        while(jsonArray.count() > 0)
-        {
-            command.append(static_cast<quint8>(jsonArray.takeAt(0).toInt()));
+        quint32 gwuid = static_cast<quint32>(jsonObj.take("gwuid").toInt(0));
+        qDebug() << "Gateway UID From Server: " << gwuid;
+        if (cGatewayUID::getGateWayUID() == gwuid) {
+            QJsonArray jsonArray = jsonObj.take("command").toArray();
+            while(jsonArray.count() > 0)
+            {
+                command.append(static_cast<quint8>(jsonArray.takeAt(0).toInt()));
+            }
         }
-//        }
     }
     return command;
 }
 
 QJsonDocument cJSONParser::createJSONToServer(QByteArray data)
 {
-    //quint32 gwuid = cGatewayUID::getGateWayUID();
+    quint32 gwuid = cGatewayUID::getGateWayUID();
     QJsonArray commandArray;
     QJsonObject itemObj;
     for (int i = 0; i < data.count(); i++) {
         commandArray.append(QJsonValue(data.at(i)));
     }
-    //itemObj.insert("gwuid", static_cast<int>(gwuid));
+    itemObj.insert("gwuid", static_cast<int>(gwuid));
     itemObj.insert("nodedata", QJsonValue(commandArray));
     QJsonDocument m_configInfoJson = QJsonDocument(itemObj);
     return m_configInfoJson;
